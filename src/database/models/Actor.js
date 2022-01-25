4da66fdeeea93097e0f85c69e1713b849474399e
DávalosLeonardo <davaloslm@hotmail.com.ar>
2022-01-25 20:34:54 -0300

    Agrega modelos

---
+++ src/database/models/Actor.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+    const Actor = sequelize.define(
+        'actors', 
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+            },
+            first_name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            last_name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            rating: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false
+            },
+            favorite_movie_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 'No tiene'
+            },
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            timestamps: false
+        })
+        return Actor
+}
